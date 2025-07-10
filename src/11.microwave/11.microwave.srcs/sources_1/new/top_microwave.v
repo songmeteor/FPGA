bff54b790cd232ca017a997c12aa15d4b5abd79e
songmeteor <meteor520@naver.com>
2025-07-10 21:47:14 +0900

    11.microwave 추가

---
+++ src/11.microwave/11.microwave.srcs/sources_1/new/top_microwave.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/07/10 21:39:56
+// Design Name: 
+// Module Name: top_microwave
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_microwave(
+
+    );
+endmodule
