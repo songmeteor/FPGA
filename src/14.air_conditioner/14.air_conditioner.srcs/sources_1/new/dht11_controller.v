7c739b1c88891c610849c3249b7d953106277827
songmeteor <meteor520@naver.com>
2025-07-20 17:45:25 +0900

    11

--- src/14.air_conditioner/14.air_conditioner.srcs/sources_1/new/dht11_controller.v
+++ src/14.air_conditioner/14.air_conditioner.srcs/sources_1/new/dht11_controller.v
@@ -545,9 +545,7 @@ module dht11_controller(
                     timer_count <= timer_count + 1;
                     if (dht11_data == 1'b0) begin
                         state <= DATA_PROCESS;
-                    end else begin
-                        state <= DATA_MEASURE_HIGH; 
-                    end
+                    end 
                 end
 
                 // DATA_MEASURE_HIGH : begin
